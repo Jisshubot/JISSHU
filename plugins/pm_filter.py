48f9d513dbe8209d7c9e0a8a98509883af33e50b
JISSHU_BOT <142872501+Jisshubot@users.noreply.github.com>
2023-10-28 21:53:59 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -50,7 +50,7 @@ async def pm_next_page(bot, query):
             btn = [[InlineKeyboardButton(text=f"💖{file.file_name}", callback_data=f'pmfile#{file.file_id}'),
                     InlineKeyboardButton(text=f"💖{get_size(file.file_size)}", callback_data=f'pmfile#{file.file_id}')] for file in files ]
 
-    btn.insert(0, [InlineKeyboardButton("💖 JOIN BACKUP 💖", "https://t.me/MAP_Originals_Backup")])
+    btn.insert(0, [InlineKeyboardButton("💖 JOIN BACKUP 💖", url="https://t.me/MAP_Originals_Backup")])
     if 0 < offset <= 10: off_set = 0
     elif offset == 0: off_set = None
     else: off_set = offset - 10
@@ -124,7 +124,7 @@ async def pm_AutoFilter(client, msg, pmspoll=False):
             btn = [[InlineKeyboardButton(text=f"💖{file.file_name}", callback_data=f'{pre}#{req}#{file.file_id}'),
                     InlineKeyboardButton(text=f"💖{get_size(file.file_size)}", callback_data=f'{pre}#{file.file_id}')] for file in files ]    
 
-    btn.insert(0, [InlineKeyboardButton("💖 JOIN BACKUP 💖", "https://t.me/MAP_Originals_Backup")])
+    btn.insert(0, [InlineKeyboardButton("💖 JOIN BACKUP 💖", url="https://t.me/MAP_Originals_Backup")])
     if offset != "":
         key = f"{message.id}"
         temp.PM_BUTTONS[key] = search
